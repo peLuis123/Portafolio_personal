b406e01d3e7443d583e0d1a6803b3c8f4b2a5417
LuisCalla1 <pedro.ramos@devitm.com>
2023-08-02 16:52:05 -0500

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/PORTAFOLIO/" : "/",
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/Portafolio_personal/" : "/",
   transpileDependencies: true,
-});
+};
